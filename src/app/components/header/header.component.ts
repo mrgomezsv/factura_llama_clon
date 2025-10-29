b5d3334a96169c40be5920a4cf6b1cbdc08c6582
Mario Roberto <gomez@thetecwave.com>
2025-10-29 10:13:39 -0600

    FEAT: Crear Header con selector de empresa y período

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,63 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DteService } from '../../services/dte.service';
+import { Empresa } from '../../models/empresa.model';
+import { PeriodoTributario } from '../../models/periodo-tributario.model';
+import { DatePickerComponent } from '../date-picker/date-picker.component';
+import { GenerateDteMenuComponent } from '../generate-dte-menu/generate-dte-menu.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, DatePickerComponent, GenerateDteMenuComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit {
+  @Output() periodoCambiado = new EventEmitter<PeriodoTributario>();
+  
+  empresas: Empresa[] = [];
+  empresaSeleccionada: Empresa | null = null;
+  periodoSeleccionado: PeriodoTributario;
+  mostrarSelectorEmpresa: boolean = false;
+  mostrarSelectorPeriodo: boolean = false;
+
+  constructor(private dteService: DteService) {
+    this.periodoSeleccionado = PeriodoTributario.ahora();
+  }
+
+  ngOnInit(): void {
+    this.cargarEmpresas();
+  }
+
+  cargarEmpresas(): void {
+    this.dteService.getEmpresas().subscribe(empresas => {
+      this.empresas = empresas;
+      if (empresas.length > 0) {
+        this.empresaSeleccionada = empresas[0];
+      }
+    });
+  }
+
+  seleccionarEmpresa(empresa: Empresa): void {
+    this.empresaSeleccionada = empresa;
+    this.mostrarSelectorEmpresa = false;
+  }
+
+  toggleSelectorEmpresa(): void {
+    this.mostrarSelectorEmpresa = !this.mostrarSelectorEmpresa;
+    this.mostrarSelectorPeriodo = false;
+  }
+
+  toggleSelectorPeriodo(): void {
+    this.mostrarSelectorPeriodo = !this.mostrarSelectorPeriodo;
+    this.mostrarSelectorEmpresa = false;
+  }
+
+  onPeriodoSeleccionado(periodo: PeriodoTributario): void {
+    this.periodoSeleccionado = periodo;
+    this.mostrarSelectorPeriodo = false;
+    this.periodoCambiado.emit(periodo);
+  }
+}
+
