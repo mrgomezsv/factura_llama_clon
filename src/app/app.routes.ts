788f8a6cf00f8859c69bcdab7f3b0ca50b62d3b8
Mario Roberto <gomez@thetecwave.com>
2025-10-30 16:18:28 -0600

    FEAT: Crear componente Login y ruta /login; ajustar bordes del drawer para eliminar borde oscuro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: 'factura/nueva',
     loadComponent: () =>
       import('./pages/factura/factura-page.component').then(m => m.FacturaPageComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(m => m.LoginComponent)
   }
 ];
