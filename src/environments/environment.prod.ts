02d2c55081c48436891f6e754d31dd2cd843c3f5
Mario Roberto <gomez@thetecwave.com>
2025-12-09 18:19:14 -0600

    FEAT: Agregar archivos de configuración de entorno

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "TU_API_KEY_PRODUCCION",
+    authDomain: "tu-proyecto-prod.firebaseapp.com",
+    projectId: "tu-proyecto-prod",
+    storageBucket: "tu-proyecto-prod.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "TU_APP_ID_PROD"
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "TU_API_KEY_AQUI",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    projectId: "tu-proyecto-id",
+    storageBucket: "tu-proyecto.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "TU_APP_ID"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
