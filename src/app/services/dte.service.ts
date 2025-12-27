5759dd81bff33df2381e889fee049c71b11130bc
Mario Roberto <gomez@thetecwave.com>
2025-12-27 11:03:41 -0600

    FIX: Corregir mapeo de tipoDte para Comprobante de Retención

--- src/app/services/dte.service.ts
+++ src/app/services/dte.service.ts
@@ -101,7 +101,7 @@ export class DteService {
             WHEN tipo_dte = '11' THEN 'Factura de Exportación'
             WHEN tipo_dte = '14' THEN 'Factura de Sujeto Excluido'
             WHEN tipo_dte = '15' THEN 'Comprobante de Donación'
-            WHEN tipo_dte = '16' THEN 'Comprobante de Retención'
+            WHEN tipo_dte = '07' THEN 'Comprobante de Retención'
             ELSE 'Documento'
           END) as tipo,
           tipo_dte,
