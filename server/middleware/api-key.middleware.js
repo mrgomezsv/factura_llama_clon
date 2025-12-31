4ea5a0dc10c41f4ecaa82f42ee4baa52eb3f4fe7
Mario Roberto <gomez@thetecwave.com>
2025-12-30 18:11:07 -0600

    FIX: Actualizar credenciales de BD en api-key.middleware para producción

--- server/middleware/api-key.middleware.js
+++ server/middleware/api-key.middleware.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME || 'wavepos_dte_v2',
-    user: process.env.DB_USER || 'mrgomez',
-    password: process.env.DB_PASSWORD || 'Karin2100',
+    host: process.env.DB_HOST || '45.10.160.29',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || 'thetecwa1_fe_dba_prod',
+    user: process.env.DB_USER || 'thetecwa1_fe_dba_prod',
+    password: process.env.DB_PASSWORD || 'ml%BHX$C//Z$f6uL',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
